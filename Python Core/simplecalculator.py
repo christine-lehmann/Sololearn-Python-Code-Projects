52ba23b2df1e2f7f6259d16c84fa7d42bbeef1f6
Christine Cion <christinemaecion@gmail.com>
2022-03-07 13:55:12 +0800

    added simplecalculator.py

---
+++ Python Core/simplecalculator.py
@@ -0,0 +1,18 @@
+'''
+Simple Calculator
+
+
+Write a program to take two integers as input and output their sum.
+
+Sample Input:
+2
+8
+
+Sample Output:
+10
+'''
+
+a = int(input())
+b = int(input())
+
+print(a+b)
