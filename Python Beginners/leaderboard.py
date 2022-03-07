f5fd123154fbd662b751f97664e44c71a668f9de
Christine Cion <christinemaecion@gmail.com>
2022-03-07 12:34:26 +0800

    added leaderboard.py

---
+++ Python Beginners/leaderboard.py
@@ -0,0 +1,23 @@
+'''
+Strings
+
+You need to make a program for a leaderboard.
+The program needs to output the numbers 1 to 9, each on a separate line, followed by a dot:
+1.
+2.
+3.
+...'''
+
+print("""1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.""")
+
+
+#You can use the \n newline character to create line breaks, or
+#alternatively, create the desired output using three double quotes """.
